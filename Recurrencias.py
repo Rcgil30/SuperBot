3dcb1fe8bf26018118c0d6a933cac2d7fe53afa6
Rcgil30 <rcgil@uninorte.edu.co>
2023-05-20 21:33:41 -0500

    Arreglo de errores final

--- Recurrencias.py
+++ Recurrencias.py
@@ -48,7 +48,6 @@ def RelacionesDeRecurrencia(input):
   f = sp.latex('f(n) = ') + sp.latex(fn)
   # Enviamos esta fórmula de LaTeX a codecogs para tenerlo en formato de imagen
   response = requests.get(f'http://latex.codecogs.com/png.latex?\\dpi{{1200}} {f}')
-  print(response.content)
   # Devolvemos la imagen cono bytes para que el bot lo lea
   return io.BytesIO(response.content)
   
