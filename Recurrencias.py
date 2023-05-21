6b2045105483e07df91294858428ee785375548f
Rcgil30 <rcgil@uninorte.edu.co>
2023-05-20 21:29:30 -0500

    Prueba

--- Recurrencias.py
+++ Recurrencias.py
@@ -47,8 +47,8 @@ def RelacionesDeRecurrencia(input):
   # Convertimos el resultado a LaTeX
   f = sp.latex('f(n) = ') + sp.latex(fn)
   # Enviamos esta fórmula de LaTeX a codecogs para tenerlo en formato de imagen
-  response = requests.get(f'http://latex.codecogs.com/png.latex?\\dpi{{1200}} {formula}')
-  print(responde.content)
+  response = requests.get(f'http://latex.codecogs.com/png.latex?\\dpi{{1200}} {f}')
+  print(response.content)
   # Devolvemos la imagen cono bytes para que el bot lo lea
   return io.BytesIO(response.content)
   
